f09b81014f5a6acd17844241449c1ca4c80512ab
wongco <ginson@gmail.com>
2018-12-06 00:11:44 -0800

    more cleanup

--- secret.py
+++
@@ -1,7 +0,0 @@
-FLASK_SECRET_KEY = "OMG PLZ USE GITIGNORE"
-POSTGRESQL_DB = "postgres:///feedback"
-
-
-# new_admin = User(username = 'gin', password = '1234', email = 'gin@gin.com', first_name = 'Gin', last_name = 'W', is_admin = True)
-# db.session.add(new_admin)
-# db.session.commit()
